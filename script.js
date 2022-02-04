798990652603978ccc7a1b20be378b0b5469c70c
Narendra Ram <narenramm@gmail.com>
2022-02-04 13:16:48 +0530

    basic password function

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function myFunction() {
-    var copyText = document.getElementById("myInput");
+function copyPsswrd() {
+    var copyText = document.getElementById("password");
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value);
 
     var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
+    tooltip.innerHTML = "Copied";
 }
 
 function outFunc() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copy to clipboard";
 }
+
+
+var password = document.getElementById("password");
+
+/* Function to generate combination of password */
+function generatePsswrd() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 50;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+}
+
+window.onload = (event) => {
+    console.log('hello world');
+    generatePsswrd()
+};
