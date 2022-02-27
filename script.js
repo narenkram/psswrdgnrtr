7201cefc96f83318001cb7f83d3fa51374b4fffe
Narendra Ram <narenramm@gmail.com>
2022-02-27 11:06:14 +0530

    update range slider

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+var password = document.getElementById("password");
+var psswrd_range_slider;
+var psswrd_limit;
+
+/* Function to generate combination of password */
+function generatePsswrd() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    passwordLength = 50;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+}
+
+
+window.onload = (event) => {
+    console.log('hello world');
+    psswrd_range_slider = document.getElementById("psswrd_range_slider");
+    psswrd_limit = document.getElementById("psswrd_limit");
+
+
+    psswrd_range_slider.addEventListener("input", OutputPsswrdLimit);
+    function OutputPsswrdLimit() {
+        psswrd_limit.innerHTML = psswrd_range_slider.value;
+    }
+
+
+    generatePsswrd();
+
+};
+
+
 function copyPsswrd() {
     var copyText = document.getElementById("password");
     copyText.select();
@@ -12,23 +46,3 @@ function outFunc() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copy to clipboard";
 }
-
-
-var password = document.getElementById("password");
-
-/* Function to generate combination of password */
-function generatePsswrd() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 50;
-    var password = "";
-    for (var i = 0; i <= passwordLength; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
-    }
-    document.getElementById("password").value = password;
-}
-
-window.onload = (event) => {
-    console.log('hello world');
-    generatePsswrd()
-};
